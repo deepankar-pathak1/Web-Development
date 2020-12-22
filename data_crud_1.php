70c80317d3e8a1904d91399ea68f016604b31b19
deepankar-pathak1 <67847047+deepankar-pathak1@users.noreply.github.com>
2020-12-22 11:18:51 +0530

    Create data_crud_1.php
    https://www.codespeaker.com/code-snippets/create-read-update-delete-crud-php-using-mysqli/

---
+++ data_crud_1.php
@@ -0,0 +1,69 @@
+<?php
+
+include 'connect_1.php';
+
+
+?>
+
+<div class="container">
+<?php
+
+if(isset($_POST['addnew'])){
+    if( empty($_POST['firstname']) || empty($_POST['lastname'])
+        || empty($_POST['address']) || empty($_POST['contact']) )
+    {
+        echo "Please fillout all required fields";
+    }else{
+        $firstname = $_POST['firstname'];
+        $lastname = $_POST['lastname'];
+        $address = $_POST['address'];
+        $contact = $_POST['contact'];
+        $sql = "INSERT INTO users(firstname,lastname,address,contact)
+        VALUES('$firstname','$lastname','$address','$contact')";
+		
+		
+		if( $conn->query($sql) === TRUE){
+            echo "<div class='alert alert-success'>Successfully added new user</div>";
+        }else{
+            echo "<div class='alert alert-danger'>Error: There was an error while adding new user</div>";
+        }
+    }
+}
+?>
+<html>
+<head>
+<style>
+<style>
+
+
+</style>
+</style>
+<meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Sofia|Trirong">
+
+</head>
+<div class="row">
+    <div class="col-md-6 col-md-offset-3">
+        <div class="box">
+        <h3><i class="glyphicon glyphicon-plus"></i>&nbsp;Add New User</h3>
+        <form action="" method="POST">
+        <label for="firstname">Firstname</label>
+        <input type="text" id="firstname" name="firstname" class="form-control"><br>
+        <label for="lastname">Lastname</label>
+        <input type="text" name="lastname" id="lastname" class="form-control"><br>
+        <label for="address">Address</label>
+        <textarea rows="4" name="address" class="form-control"></textarea><br>
+        <label for="contact">Contact</label>
+        <input type="text" name="contact" id="contact" class="form-control"><br>
+        <br>
+        <input type="submit" name="addnew" class="btn btn-success" value="Add New">
+        </form>
+    </div>
+    </div>
+    </div>
+</div>
+</html>
